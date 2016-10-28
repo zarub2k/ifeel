a6cccfcd3674b4af674d91fff4a927e41938b215
Thamizharasu <zarub2k@gmail.com>
2016-10-28 11:04:22 +0530

    Navigation code is added

--- app/src/main/java/com/cloudskol/ifeel/MainActivity.java
+++ app/src/main/java/com/cloudskol/ifeel/MainActivity.java
@@ -1,7 +1,12 @@
 package com.cloudskol.ifeel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        renderUi();
+    }
+
+    private void renderUi() {
+        ListView featuresListView = (ListView) findViewById(R.id.features_list);
+        featuresListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switchActivity(position);
+            }
+        });
+    }
+
+    private void switchActivity(int index) {
+        switch (index) {
+            case 0:
+                switchFeelActivity();
+                break;
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+        }
+    }
+
+    private void switchFeelActivity() {
+        final Intent intent = new Intent(this, FeelActivity.class);
+        startActivity(intent);
     }
 }
