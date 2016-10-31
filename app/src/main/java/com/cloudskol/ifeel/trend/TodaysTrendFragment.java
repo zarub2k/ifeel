4066e8baa40b540f1b9184738ca7ed43605fb550
Thamizharasu <zarub2k@gmail.com>
2016-10-31 23:13:21 +0530

    Today's trend is rendered in the fragment

--- app/src/main/java/com/cloudskol/ifeel/trend/TodaysTrendFragment.java
+++ app/src/main/java/com/cloudskol/ifeel/trend/TodaysTrendFragment.java
@@ -10,6 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cloudskol.ifeel.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tham
@@ -25,6 +33,40 @@ public class TodaysTrendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_todays_trend, container, false);
+        final View view = inflater.inflate(R.layout.fragment_todays_trend, container, false);
+        renderPieChart(view);
+        return view;
+    }
+
+    private void renderPieChart(View view) {
+        PieChart todayTrend = (PieChart) view.findViewById(R.id.todayTrend);
+        todayTrend.setUsePercentValues(true);
+        todayTrend.getDescription().setEnabled(false);
+
+
+        List<PieEntry> entries = new ArrayList<PieEntry>(8);
+
+        entries.add(new PieEntry(10, "Fear"));
+        entries.add(new PieEntry(12, "Anger"));
+        entries.add(new PieEntry(8, "Sad"));
+        entries.add(new PieEntry(20, "Joy"));
+        entries.add(new PieEntry(3, "Disgust"));
+        entries.add(new PieEntry(5, "Surprised"));
+
+        final PieDataSet pieDataSet = new PieDataSet(entries, "Today");
+
+        List<Integer> colors = new ArrayList<>(8);
+        for (int color : ColorTemplate.COLORFUL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color : ColorTemplate.JOYFUL_COLORS) {
+            colors.add(color);
+        }
+
+        pieDataSet.setColors(colors);
+
+        todayTrend.setData(new PieData(pieDataSet));
+        todayTrend.invalidate();
     }
 }
