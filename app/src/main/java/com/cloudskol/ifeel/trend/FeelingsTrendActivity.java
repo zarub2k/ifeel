858b0268d358a505040d4d05ec82be9be9aed269
Thamizharasu S <zarub2k@gmail.com>
2016-11-02 19:14:07 +0530

    Unused imports removed

--- app/src/main/java/com/cloudskol/ifeel/trend/FeelingsTrendActivity.java
+++ app/src/main/java/com/cloudskol/ifeel/trend/FeelingsTrendActivity.java
@@ -3,17 +3,13 @@ package com.cloudskol.ifeel.trend;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
 import com.cloudskol.ifeel.R;
-import com.cloudskol.ifeel.influencer.InfluencerPagerAdapter;
 
 public class FeelingsTrendActivity extends AppCompatActivity {
     private static final String LOG_TAG = FeelingsTrendActivity.class.getSimpleName();
