6718e156e5a81cb607f680056a37367b984350c9
Thamizharasu S <zarub2k@gmail.com>
2016-11-03 18:00:31 +0530

    Alignment changed

--- app/src/main/java/com/cloudskol/ifeel/trend/TodaysTrendFragment.java
+++ app/src/main/java/com/cloudskol/ifeel/trend/TodaysTrendFragment.java
@@ -32,10 +32,8 @@ public class TodaysTrendFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_todays_trend, container, false);
-
         renderPieChart(view);
 
         return view;
