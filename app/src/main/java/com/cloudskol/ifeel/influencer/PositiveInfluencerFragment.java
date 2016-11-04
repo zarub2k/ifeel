6679f6c23ebb6196f698dfd8fc04ab6e7e0dd3fa
Thamizharasu <zarub2k@gmail.com>
2016-11-04 23:31:58 +0530

    Positive influencer Ui is completed

--- app/src/main/java/com/cloudskol/ifeel/influencer/PositiveInfluencerFragment.java
+++ app/src/main/java/com/cloudskol/ifeel/influencer/PositiveInfluencerFragment.java
@@ -8,8 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cloudskol.ifeel.R;
+import com.cloudskol.ifeel.trend.TrendAggregationByFeeling;
+import com.cloudskol.ifeel.trend.TrendQueryManager;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,13 +39,57 @@ public class PositiveInfluencerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_positive_influencer, container, false);
 
-        final List<InfluencerAggregation> influencers = InfluencerQueryManager.getInstance(
-                this.getContext()).getInfluencers();
+        renderPieChart(view);
 
         return view;
     }
 
+    private void renderPieChart(View view) {
+        PieChart monthlyTrend = (PieChart) view.findViewById(R.id.positiveInfluencer);
+        setChartConfiguration(monthlyTrend);
+
+        monthlyTrend.setData(getChartData());
+        monthlyTrend.notifyDataSetChanged();
+        monthlyTrend.invalidate();
+    }
+
+    private PieData getChartData() {
+        final List<InfluencerAggregation> influencers = InfluencerQueryManager.getInstance(
+                this.getContext()).getInfluencers();
+
+        final Map<String, Integer> positiveByPerson = InfluencerAggregator.getInstance().getPositiveByPerson(influencers);
+
+
+        List<PieEntry> entries = new ArrayList<PieEntry>(positiveByPerson.size());
+        final Set<Map.Entry<String, Integer>> personEntrySet = positiveByPerson.entrySet();
+        for (Map.Entry<String, Integer> entry : personEntrySet) {
+            entries.add(new PieEntry(entry.getValue(), entry.getKey()));
+        }
+
+        final PieDataSet pieDataSet = new PieDataSet(entries, null);
+
+        List<Integer> colors = new ArrayList<>(8);
+        for (int color : ColorTemplate.JOYFUL_COLORS) {
+            colors.add(color);
+        }
+
+        pieDataSet.setColors(colors);
+        pieDataSet.setSliceSpace(3f);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieData.setValueFormatter(new PercentFormatter());
+
+        return pieData;
+    }
+
+    private void setChartConfiguration(PieChart pieChart) {
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+        pieChart.setDrawCenterText(true);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        pieChart.highlightValues(null);
+    }
 }
